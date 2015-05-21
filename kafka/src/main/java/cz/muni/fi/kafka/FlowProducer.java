13245a3d5c6dc28aeabc152949176036f3edab09
radozaj <radozaj@centrum.sk>
2015-05-21 15:02:05 +0200

    edit start kafka producer with args

--- kafka/src/main/java/cz/muni/fi/kafka/FlowProducer.java
+++ kafka/src/main/java/cz/muni/fi/kafka/FlowProducer.java
@@ -8,16 +8,21 @@ import kafka.producer.ProducerConfig;
 
 public class FlowProducer {
     public static void main(String[] args) {
-        if (args.length < 2) {
-            //throw new IllegalArgumentException("Missing argument: input_file or name_of_topic." );
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Missing argument: input_file [name_of_topic]" );
         }
-        String inputFile = "/mnt/data/radozaj/Masarykova univerzita/Magisterske studium/diplomovka/out1k"; //args[0];
-        String topic = "securitycloud-testing-data"; //args[1];
-
+        String inputFile = args[0];
+        
+        String topic;
+        if (args.length >= 2) {
+             topic = args[1];
+        } else {
+            topic = "securitycloud-testing-data";
+        }
+        
         Properties props = new Properties();
         props.put("metadata.broker.list", "localhost:9092");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
-        //props.put("partitioner.class", "cz.muni.fi.kafka.SimplePartitioner");
         props.put("request.required.acks", "1");
 
         ProducerConfig config = new ProducerConfig(props);
