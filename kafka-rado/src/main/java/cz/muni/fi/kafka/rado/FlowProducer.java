025b646c2998b2c37426e422bab04a0aec83ea53
radozaj <radozaj@centrum.sk>
2015-06-22 19:25:39 +0200

    kafka-rado: round-robin partitioning

--- kafka-rado/src/main/java/cz/muni/fi/kafka/rado/FlowProducer.java
+++ kafka-rado/src/main/java/cz/muni/fi/kafka/rado/FlowProducer.java
@@ -23,6 +23,7 @@ public class FlowProducer {
         Properties props = new Properties();
         props.put("metadata.broker.list", "localhost:9092");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("partitioner.class", "cz.muni.fi.kafka.rado.RoundRobinPartitioner");
         props.put("request.required.acks", "0");
         props.put("producer.type", "async");
 
